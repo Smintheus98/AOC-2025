08b4f0112af90efeb109df6a0103e2ff8571faa4
Yannic Kitten <yannic.kitten@outlook.de>
2025-12-04 08:33:18 +0100

    Add argmax

--- lib/utils.nim
+++ lib/utils.nim
@@ -4,3 +4,15 @@ proc div2*[I: SomeInteger](i: I): I =
 
 proc mod2*[I: SomeInteger](i: I): I =
   i and 1
+
+proc argmax*[I: SomeInteger](s: openArray[I]): tuple[value: I; location: int] =
+  if s.len == 0:
+    return (-1, -1)
+  var
+    maxVal: I = s[0]
+    maxLoc: int = 0
+  for i, v in s:
+    if v > maxVal:
+      maxVal = v
+      maxLoc = i
+  return (maxVal, maxLoc)
